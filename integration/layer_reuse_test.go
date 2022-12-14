c66ed282f88fd07597ad5f72632dea965f774fd1
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-12-14 16:19:40 -0500

    Removes 3.1 version line (#43)
    Co-authored-by: Sophie Wigmore <swigmore@vmware.com>

--- integration/layer_reuse_test.go
+++ integration/layer_reuse_test.go
@@ -171,7 +171,7 @@ func testLayerReuse(t *testing.T, context spec.G, it spec.S) {
 					settings.Buildpacks.BuildPlan.Online,
 				).
 				WithEnv(map[string]string{
-					"BP_DOTNET_FRAMEWORK_VERSION": "3.*",
+					"BP_DOTNET_FRAMEWORK_VERSION": "7.*",
 				}).
 				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred(), logs.String())
